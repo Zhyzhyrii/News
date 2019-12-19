6aadbdfbb0a860f070f8d1c9b8993aa4798ae4e6
IgorZ <izhizhirii@gmail.com>
2019-12-19 21:33:16 +0200

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Игорь on 19.12.2019.
+//  Copyright © 2019 Igor Zhyzhyrii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
