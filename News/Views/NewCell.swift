05726850e8b58fdde71fd9c480f4fa0b49c180c5
IgorZ <izhizhirii@gmail.com>
2020-01-25 15:27:15 +0200

    Implement caching the images

--- News/Views/NewCell.swift
+++ News/Views/NewCell.swift
@@ -19,18 +19,7 @@ class NewCell: UITableViewCell {
         newTitleLabel.text = newData.title
         newTextLabel.text = newData.descripton
         newTextLabel.isHidden = true
-        
-        if let imageData = newData.imageRef {
-            guard let url = URL(string: imageData) else { return }
-            DispatchQueue.global().async { //TODO - ??about dispatch
-                guard let data = try? Data(contentsOf: url) else { return }
-                DispatchQueue.main.async {
-                    self.newImageView.image = UIImage(data: data)
-                }
-            }
-        } else {
-            print("Default image") //TODO
-        }
+        newImageView.fetchImage(with: newData.imageRef)
         
     }
 }
